7d9b8214d9496a352d31c57666bf45c0e69aee82
happy956 <happy8604@naver.com>
2022-12-10 19:38:53 +0900

    change fetch url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Trip from "./components/Trip";
 import "./css/app.css";
 
 const getData = (name) => {
-  const res = axios.get(`./data/${name}.json`);
+  const res = axios.get(`https://raw.githubusercontent.com/HNU209/Kaist-campus/main/src/data/${name}.json`);
   const data = res.then((r) => r.data);
   return data;
 };
