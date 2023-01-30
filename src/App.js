10e66b5d8faa91f00028c664505d6569417cf040
happy956 <happy8604@naver.com>
2023-01-30 18:23:54 +0900

    update all code file and data

--- src/App.js
+++ src/App.js
@@ -1,72 +1,58 @@
 import "mapbox-gl/dist/mapbox-gl.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import Trip from "./components/Trip";
+
 import Splash from "./components/Splash";
+import Trip from "./components/Trip";
 import "./css/app.css";
 
-const getData = (name) => {
+const fetchData = (name) => {
   const res = axios.get(`https://raw.githubusercontent.com/HNU209/Kaist-campus/main/src/data/${name}.json`);
   const data = res.then((r) => r.data);
   return data;
 };
 
 const App = () => {
-  const minTime = 480;
-  const maxTime = 660;
-  const [time, setTime] = useState(minTime);
   const [electricCarTrip, setElectricCarTrip] = useState([]);
   const [busTrip, setBusTrip] = useState([]);
   const [busFoot, setBusFoot] = useState([]);
   const [busStopLoc, setBusStopLoc] = useState([]);
   const [busPath, setBusPath] = useState([]);
-  const [loaded, setLoaded] = useState(false);
+  const [isloaded, setIsLoaded] = useState(false);
+
+  const getData = useCallback(async () => {
+    const electricCarTrip = await fetchData("electric_car_trip");
+    const busTrip = await fetchData("shuttle_bus_trip");
+    const busFoot = await fetchData("shuttle_bus_foot_trip");
+    const busStopLoc = await fetchData("bus_stop_point");
+    const busPath = await fetchData("bus_stop_path");
+
+    setElectricCarTrip((prev) => electricCarTrip);
+    setBusTrip((prev) => busTrip);
+    setBusFoot((prev) => busFoot);
+    setBusStopLoc((prev) => busStopLoc);
+    setBusPath((prev) => busPath);
+    setIsLoaded(true);
+  }, []);
 
   useEffect(() => {
-    const getFetchData = async () => {
-      const electricCarTrip = await getData("electric_car_trip");
-      const busTrip = await getData("bus_trip");
-      const busFoot = await getData("bus_foot");
-      const busStopLoc = await getData("bus_stop_point");
-      const busPath = await getData("bus_stop_path");
-
-      if (electricCarTrip && busStopLoc && busPath && busTrip && busFoot) {
-        setElectricCarTrip((prev) => electricCarTrip);
-        setBusTrip((prev) => busTrip);
-        setBusFoot((prev) => busFoot);
-        setBusStopLoc((prev) => busStopLoc);
-        setBusPath((prev) => busPath);
-        setLoaded(true);
-      }
-    };
-
-    getFetchData();
-  }, []);
+    getData();
+  }, [getData]);
 
   return (
     <div className="container">
-      {loaded ? (
+      {!isloaded && <Splash />}
+      {isloaded && (
         <>
           <Trip
+            name={"버스"}
             busTrip={busTrip}
             busFoot={busFoot}
             busStopLoc={busStopLoc}
             busPath={busPath}
-            minTime={minTime}
-            maxTime={maxTime}
-            time={time}
-            setTime={setTime}
           ></Trip>
-          <Trip
-            electricCarTrip={electricCarTrip}
-            minTime={minTime}
-            maxTime={maxTime}
-            time={time}
-            setTime={setTime}
-          ></Trip>
+          <Trip name={"초소형 전기차"} electricCarTrip={electricCarTrip}></Trip>
         </>
-      ) : (
-        <Splash></Splash>
       )}
     </div>
   );
--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,187 +1,192 @@
-import React, { useState, useEffect } from 'react';
-import DeckGL from '@deck.gl/react';
-import { Map } from 'react-map-gl';
-import {AmbientLight, PointLight, LightingEffect} from '@deck.gl/core';
-import { TripsLayer } from '@deck.gl/geo-layers';
-import { IconLayer, PathLayer } from '@deck.gl/layers';
+import React, { useState, useEffect, useCallback } from "react";
+
+import DeckGL from "@deck.gl/react";
+import { AmbientLight, PointLight, LightingEffect } from "@deck.gl/core";
+import { IconLayer, PathLayer } from "@deck.gl/layers";
+import { TripsLayer } from "@deck.gl/geo-layers";
+import { Map } from "react-map-gl";
+
 import Slider from "@mui/material/Slider";
-import '../css/trip.css';
-import legend from '../img/legend.png';
+import legend from "../img/legend.png";
+import "../css/trip.css";
 
 const ambientLight = new AmbientLight({
-    color: [255, 255, 255],
-    intensity: 1.0
+  color: [255, 255, 255],
+  intensity: 1.0,
 });
-    
+
 const pointLight = new PointLight({
-    color: [255, 255, 255],
-    intensity: 2.0,
-    position: [-74.05, 40.7, 8000]
+  color: [255, 255, 255],
+  intensity: 2.0,
+  position: [-74.05, 40.7, 8000],
 });
-  
-const lightingEffect = new LightingEffect({ambientLight, pointLight});
-  
-const material = {
-    ambient: 0.1,
-    diffuse: 0.6,
-    shininess: 32,
-    specularColor: [60, 64, 70]
-};
-  
+
+const lightingEffect = new LightingEffect({ ambientLight, pointLight });
+
 const DEFAULT_THEME = {
-    buildingColor: [74, 80, 87],
-    trailColor0: [253, 128, 93],
-    trailColor1: [23, 184, 190],
-    material,
-    effects: [lightingEffect]
+  buildingColor: [74, 80, 87],
+  trailColor0: [253, 128, 93],
+  trailColor1: [23, 184, 190],
+  effects: [lightingEffect],
 };
 
 const INITIAL_VIEW_STATE = {
-    longitude: 127.38,
-    latitude: 36.38,
-    zoom: 10,
-    minZoom: 12,
-    maxZoom: 20,
-    pitch: 0,
-    bearing: 0
+  longitude: 127.38,
+  latitude: 36.38,
+  zoom: 10,
+  minZoom: 12,
+  maxZoom: 20,
+  pitch: 0,
+  bearing: 0,
 };
-
-const mapStyle = 'mapbox://styles/spear5306/ckzcz5m8w002814o2coz02sjc';
-const MAPBOX_TOKEN = `pk.eyJ1Ijoic3BlYXI1MzA2IiwiYSI6ImNremN5Z2FrOTI0ZGgycm45Mzh3dDV6OWQifQ.kXGWHPRjnVAEHgVgLzXn2g`; // eslint-disable-line
 
 const ICON_MAPPING = {
-    marker: {x: 0, y: 0, width: 128, height: 128, mask: true}
+  marker: { x: 0, y: 0, width: 128, height: 128, mask: true },
 };
 
-const Trip = props => {
-    const minTime = props.minTime;
-    const maxTime = props.maxTime;
-    const time = props.time;
-    const animationSpeed = 1;
+const minTime = 480;
+const maxTime = 660;
+const animationSpeed = 1;
+const mapStyle = "mapbox://styles/spear5306/ckzcz5m8w002814o2coz02sjc";
+const MAPBOX_TOKEN = `pk.eyJ1Ijoic3BlYXI1MzA2IiwiYSI6ImNremN5Z2FrOTI0ZGgycm45Mzh3dDV6OWQifQ.kXGWHPRjnVAEHgVgLzXn2g`; // eslint-disable-line
 
-    const electricCarTrip = props.electricCarTrip;
-    const busTrip = props.busTrip;
-    const busFoot = props.busFoot;
-    const busStopLoc = props.busStopLoc;
-    const busPath = props.busPath;
+const returnAnimationTime = (time) => {
+  if (time > maxTime) {
+    return minTime;
+  } else {
+    return time + 0.01 * animationSpeed;
+  }
+};
 
-    const [animationFrame, setAnimationFrame] = useState('');
+const addZeroFill = (value) => {
+  const valueString = value.toString();
+  return valueString.length < 2 ? "0" + valueString : valueString;
+};
 
-    const animate = () => {
-        props.setTime(time => {
-        if (time > maxTime) {
-            return minTime;
-        } else {
-            return time + (0.01) * animationSpeed;
-        };
-        });
-        const af = window.requestAnimationFrame(animate);
-        setAnimationFrame(af);
-    };
+const returnAnimationDisplayTime = (time) => {
+  const hour = addZeroFill(parseInt((Math.round(time) / 60) % 24));
+  const minute = addZeroFill(Math.round(time) % 60);
+  return [hour, minute];
+};
 
-    const layers = [
-        new PathLayer({
-            id: 'bus-stop-path',
-            data: busPath,
-            pickable: true,
-            widthScale: 1,
-            widthMinPixels: 2,
-            getPath: d => d.path,
-            getColor: d => [255, 255, 0],
-            getWidth: d => 1
-        }),
-        new TripsLayer({
-            id: 'bus-trip',
-            data: busTrip,
-            getPath: d => d.trip,
-            getTimestamps: d => d.timestamp,
-            getColor: d => [255, 0, 0],
-            opacity: 1,
-            widthMinPixels: 5,
-            trailLength: 1,
-            rounded: true,
-            currentTime: time,
-            shadowEnabled: false,
-        }),
-        new TripsLayer({
-            id: 'bus-foot',
-            data: busFoot,
-            getPath: d => d.trip,
-            getTimestamps: d => d.timestamp,
-            getColor: [255, 255, 255],
-            opacity: 1,
-            widthMinPixels: 5,
-            trailLength: 1,
-            rounded: true,
-            currentTime: time,
-            shadowEnabled: false,
-        }),
-        new TripsLayer({
-            id: 'car-trip',
-            data: electricCarTrip,
-            getPath: d => d.trip,
-            getTimestamps: d => d.timestamp,
-            getColor: d => d.type === 'electric_car' ? [0, 0, 255] : [255, 255, 255],
-            opacity: 1,
-            widthMinPixels: 5,
-            trailLength: 1,
-            rounded: true,
-            currentTime: time,
-            shadowEnabled: false,
-        }),
-        new IconLayer({
-            id: 'bus-stop-point',
-            data: busStopLoc,
-            pickable: false,
-            iconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
-            iconMapping: ICON_MAPPING,
-            sizeMinPixels: 20,
-            sizeMaxPixels: 20,
-            sizeScale: 5,
-            getIcon: d => 'marker',
-            getPosition: d => d.loc,
-            getSize: d => 10,
-            getColor: d => [255, 255, 0]
-        }),
-    ];
+const Trip = (props) => {
+  const [time, setTime] = useState(minTime);
+  const [animation] = useState([]);
 
-    useEffect(() => {
-        animate();
-        return () => window.cancelAnimationFrame(animationFrame);
-    }, []);
+  const electricCarTrip = props.electricCarTrip;
+  const busTrip = props.busTrip;
+  const busFoot = props.busFoot;
+  const busStopLoc = props.busStopLoc;
+  const busPath = props.busPath;
 
-    const SliderChange = (value) => {
-        const time = value.target.value;
-        props.setTime(time);
-    };
+  const animate = useCallback(() => {
+    setTime((time) => returnAnimationTime(time));
+    animation.id = window.requestAnimationFrame(animate);
+  }, [animation]);
 
-    return (
-        <div className='trip-container' style={{position: 'relative'}}>
-            <DeckGL
-                effects={DEFAULT_THEME.effects}
-                initialViewState={INITIAL_VIEW_STATE}
-                controller={true}
-                layers={layers}
-            >
-                <Map
-                mapStyle={mapStyle}
-                mapboxAccessToken={MAPBOX_TOKEN}
-                />
-            </DeckGL>
-            <h1 className='time'>
-                TIME : {(String(parseInt(Math.round(time) / 60) % 24).length === 2) ? parseInt(Math.round(time) / 60) % 24 : '0'+String(parseInt(Math.round(time) / 60) % 24)} : {(String(Math.round(time) % 60).length === 2) ? Math.round(time) % 60 : '0'+String(Math.round(time) % 60)}
-            </h1>
-            <Slider
-                id="slider"
-                value={time}
-                min={minTime}
-                max={maxTime}
-                onChange={SliderChange}
-                track="inverted"
-            />
-            <img className='legend' src={legend}></img>
-        </div>
-    )
-}
+  useEffect(() => {
+    animation.id = window.requestAnimationFrame(animate);
+    return () => window.cancelAnimationFrame(animation.id);
+  }, [animation, animate]);
+
+  const layers = [
+    new TripsLayer({
+      id: "bus-trip",
+      data: busTrip,
+      getPath: (d) => d.trip,
+      getTimestamps: (d) => d.timestamp,
+      getColor: (d) => [255, 0, 0],
+      opacity: 1,
+      widthMinPixels: 5,
+      trailLength: 1,
+      currentTime: time,
+      shadowEnabled: false,
+    }),
+    new TripsLayer({
+      id: "bus-foot",
+      data: busFoot,
+      getPath: (d) => d.trip,
+      getTimestamps: (d) => d.timestamp,
+      getColor: [255, 255, 255],
+      opacity: 1,
+      widthMinPixels: 5,
+      trailLength: 1,
+      currentTime: time,
+      shadowEnabled: false,
+    }),
+    new TripsLayer({
+      id: "car-trip",
+      data: electricCarTrip,
+      getPath: (d) => d.trip,
+      getTimestamps: (d) => d.timestamp,
+      getColor: (d) =>
+        d.type === "electric_car" ? [0, 0, 255] : [255, 255, 255],
+      opacity: 1,
+      widthMinPixels: 5,
+      trailLength: 1,
+      currentTime: time,
+      shadowEnabled: false,
+    }),
+    new PathLayer({
+      id: "bus-stop-path",
+      data: busPath,
+      pickable: true,
+      widthScale: 1,
+      widthMinPixels: 2,
+      getPath: (d) => d.path,
+      getColor: (d) => [255, 255, 0],
+      getWidth: (d) => 1,
+    }),
+    new IconLayer({
+      id: "bus-stop-point",
+      data: busStopLoc,
+      pickable: false,
+      iconAtlas:
+        "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png",
+      iconMapping: ICON_MAPPING,
+      sizeMinPixels: 20,
+      sizeMaxPixels: 20,
+      sizeScale: 5,
+      getIcon: (d) => "marker",
+      getPosition: (d) => d.loc,
+      getSize: (d) => 10,
+      getColor: (d) => [255, 255, 0],
+    }),
+  ];
+
+  const SliderChange = (value) => {
+    const time = value.target.value;
+    setTime(time);
+  };
+
+  const [hour, minute] = returnAnimationDisplayTime(time);
+
+  return (
+    <div className="trip-container" style={{ position: "relative" }}>
+      <DeckGL
+        effects={DEFAULT_THEME.effects}
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        layers={layers}
+      >
+        <Map mapStyle={mapStyle} mapboxAccessToken={MAPBOX_TOKEN} />
+      </DeckGL>
+      <h1 className="time">
+        TIME : {`${hour} : ${minute}`}
+        <br />
+        {props.name}
+      </h1>
+      <Slider
+        id="slider"
+        value={time}
+        min={minTime}
+        max={maxTime}
+        onChange={SliderChange}
+        track="inverted"
+      />
+      <img className="legend" src={legend} alt="legend"></img>
+    </div>
+  );
+};
 
 export default Trip;
